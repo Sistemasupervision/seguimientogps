409f17d115c25bb742fe75f1ded56eccff853552
Sistemasupervision <cpnbtecnozulia@gmail.com>
2025-12-12 17:06:24 -0400

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pnb.sistema.gps',
+  appName: 'Sistema PNB GPS',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
